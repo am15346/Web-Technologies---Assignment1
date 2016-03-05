65d361d39a2d61291a6685939bd38e9d5730e504
Laser Ozdonmez <lo1368@my.bristol.ac.uk>
2016-03-05 22:46:54 +0000

    The board navigation is nicely formatted.

--- server.js
+++ server.js
@@ -48,19 +48,19 @@ var types = {
 function start() {
     test();
     var httpService = http.createServer(serve);
-    httpService.listen(ports[0], '192.168.137.1');
+    httpService.listen(ports[0], '137.222.228.4');
     var options = { key: key, cert: cert };
     var httpsService = https.createServer(options, serve);
-    httpsService.listen(ports[1], '192.168.137.1');
+    httpsService.listen(ports[1], '137.222.228.4');
     printAddresses();
 }
 
 // Print out the server addresses.
 function printAddresses() {
-    var httpAddress = "http://192.168.137.1";
+    var httpAddress = "http://137.222.228.4";
     if (ports[0] != 80) httpAddress += ":" + ports[0];
     httpAddress += "/";
-    var httpsAddress = "https://192.168.137.1";
+    var httpsAddress = "https://137.222.228.4";
     if (ports[1] != 443) httpsAddress += ":" + ports[1];
     httpsAddress += "/";
     console.log('Server running at', httpAddress, 'and', httpsAddress);
